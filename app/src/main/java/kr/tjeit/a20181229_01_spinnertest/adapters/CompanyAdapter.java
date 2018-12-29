c62e167be0be578c2feb144a6398417c17f90000
Hottory <gkffndnl123@naver.com>
2018-12-29 11:10:47 +0900

    getView 메쏘드 내부 작업

--- app/src/main/java/kr/tjeit/a20181229_01_spinnertest/adapters/CompanyAdapter.java
+++ app/src/main/java/kr/tjeit/a20181229_01_spinnertest/adapters/CompanyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -40,6 +42,16 @@ public class CompanyAdapter extends ArrayAdapter <Restaurant> {
 
             row = inf.inflate(R.layout.company_list_item,null);
         }
+
+        ImageView logoImg = row.findViewById(R.id.logoImg);
+        TextView nameTxt = row.findViewById(R.id.nameTxt);
+
+        Restaurant data = mList.get(position);
+
+
+        nameTxt.setText(data.getName());
+
+
        return row;
 
     }
